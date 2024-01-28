0dcc0d9b70e87f63fe9540c0f8f48c69322cc839
CCS2o23 <123149837+CCS2o23@users.noreply.github.com>
2024-01-27 19:07:27 -0500

    Create Decompress.sh

---
+++ Code/Decompress.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Directory containing the .csv.gz files
+DIRECTORY="./"
+
+# Change to the directory
+cd "$DIRECTORY"
+
+# Loop through all .csv.gz files and decompress them
+for file in *.csv.gz; do
+    echo "Decompressing $file..."
+    gunzip "$file"
+done
+
+echo "Decompression complete."
